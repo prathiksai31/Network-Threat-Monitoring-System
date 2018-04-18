701038ae31f307c3fcf652723159b5cd19020cb8
prathiksai31 <37631634+prathiksai31@users.noreply.github.com>
2018-04-18 22:51:46 +0530

    Add files via upload
    Shell script to create custom timeout to have control on the duration of execution of individual linux commands.

---
+++ script1.sh
@@ -0,0 +1,17 @@
+#@uthor Sai Prathik
+#Title: Shell script to create custom timeout to have control on the duration of execution of individual linux commands.
+
+#!/bin/bash
+
+custom_timeout() {
+  time=$1
+
+  command="/bin/sh -c \"$2\""
+
+  expect -c "set echo \"-noecho\"; set timeout $time; spawn -noecho $command; expect timeout { exit 1 } eof { exit 0 }"
+
+  if [ $? = 1 ] ; then
+    echo "Timeout after ${time} seconds"
+  fi
+
+}
